4d45a56532259f3fc309f28f688a6a91e3e533fb
manoj <manoj.keev@gmail.com>
2024-07-06 10:17:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import clevertap from "clevertap-web-sdk";
+import "./App.css";
+// import ClevertapReact from "clevertap-react";
 
 function App() {
+  clevertap.onUserLogin.push({
+    Site: {
+      Name: "Vinod Rathod", // String
+      Identity: 1230, // String or number
+      Email: "vinod@gmail.com", // Email address of the user
+      Phone: "+14155551234", // Phone (with the country code)
+      Gender: "M", // Can be either M or F
+      DOB: new Date(), // Date of Birth. Javascript Date object
+    },
+  });
+
+  clevertap.profile.push({
+    Site: {
+      Name: "Vinod Rathod", // String
+      Identity: 1230, // String or number
+      Email: "vinod@gmail.com", // Email address of the user
+      Phone: "+14155551234", // Phone (with the country code)
+      Gender: "M", // Can be either M or F
+      DOB: new Date(), // Date of Birth. Javascript Date object
+    },
+  });
+
+  const handleClick = () => {
+    clevertap.event.push("Product viewed", {
+      "Product name": "Puma shoes",
+      Category: "Mens Accessories",
+      Price: 5999,
+      Date: new Date(),
+    });
+    console.log("CleverTap Explore");
+  };
+
+  const handleClick1 = () => {
+    clevertap.event.push("Strategy Subscribed", {
+      "Strategy Name": "Casio Watch ",
+      price: "100",
+    });
+    console.log("CleverTap Subscribe");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is a test project for testing purposes</h1>
+      <button onClick={() => handleClick()}>CleverTap Explore</button>
+      <button onClick={() => handleClick1()}>CleverTap Subscribe</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import clevertap from "clevertap-web-sdk";
+
+clevertap.spa = true;
+// clevertap.init("844-445-ZZ6Z");
+clevertap.init("TEST-KKW-488-K86Z");
+
+clevertap.setLogLevel(3);
+//  0: disable all logs
+//  1: display only errors
+//  2: display errors and info
+//  3: display all logs
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
